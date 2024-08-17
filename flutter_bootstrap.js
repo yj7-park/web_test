39c29fcc64f7508e752cbd04654025ac37974fb2
Yongjun Park <shaneangel@naver.com>
2024-08-17 22:46:24 +0900

    관리자대시보드에 "수강전환" 부분이 미신청/수강신청 상태가 아닌 경우 (수강 중, 홀드, 수강 완료) 인 경우 녹색 표시되도록 보완, 포인트 사용시 금액 산정 미동작 버그 수정, 관리자학생정보 D-day 관련 버그 수정, #2 계정 마이페이지 로딩되지 않는 버그 수정

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3058646548"
+    serviceWorkerVersion: "3292369386"
   }
 });
